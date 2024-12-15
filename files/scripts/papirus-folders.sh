88098fe50930f94541864618f7380f5914290b40
Nicolas Castillo <code@ncastillo.xyz>
2024-12-15 11:08:32 -0500

    feat: change color scheme

--- files/scripts/papirus-folders.sh
+++ files/scripts/papirus-folders.sh
@@ -4,6 +4,6 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-/usr/bin/papirus-folders -C bluegrey --theme Papirus-Dark
-/usr/bin/papirus-folders -C bluegrey --theme Papirus
-/usr/bin/papirus-folders -C bluegrey --theme ePapirus
+/usr/bin/papirus-folders -C violet --theme Papirus-Dark
+/usr/bin/papirus-folders -C violet --theme Papirus
+/usr/bin/papirus-folders -C violet --theme ePapirus
